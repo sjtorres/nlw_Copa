cb1f9a0f2335bda71448ddf7c87362e6d7713cc7
DamaraLucenae <damara.ti.dev@gmail.comm>
2022-11-03 20:19:49 -0300

    creating routes and accesses

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+
+import { Loading } from "./src/components/Loading";
+import { Signin } from "./src/screens/Signin";
+
+import { THEME } from "./src/styles/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Signin /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
---
+++ mobile/src/screens/Signin/index.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export const Signin = () => {
+  return (
+    <Center flex={1}>
+      <Text color="white" fontSize={24} fontFamily="heading">
+        Signin
+      </Text>
+    </Center>
+  );
+};
